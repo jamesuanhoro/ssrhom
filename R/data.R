57fa70fe0bc05bafdfdb985ffaeb8a6e1dc5b683
James Uanhoro <James.Uanhoro@gmail.com>
2024-08-20 15:58:34 -0500

    adding data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Dataset from Tasky et al. (2008)
+#'
+#' @format ## `tasky`
+#' A data frame with 70 rows and 3 colums:
+#' \describe{
+#'   \item{person}{The case label}
+#'   \item{phase}{Intervention phase where "B" is treatment}
+#'   \item{count}{The count of on-task intervals out of 6}
+#'   \item{time}{Session number}
+#' }
+#' @source Tasky et al. (2008). Using Choice to Increase On-Task Behavior in
+#' Individuals with Traumatic Brain Injury. Journal of Applied Behavior
+#' Analysis, 41(2), 261–265. <https://doi.org/10.1901/jaba.2008.41-261>
+"tasky"
